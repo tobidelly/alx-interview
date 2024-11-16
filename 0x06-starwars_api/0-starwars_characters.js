bc33761892d71d051ce1728026b0fb7de5179686
tobidelly <tobidelly@gmail.com>
2024-11-16 07:59:41 +0000

    update 0-starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,35 +1,17 @@
 #!/usr/bin/node
-// script that prints all characters of a Star Wars movie
+
 const request = require('request');
-const filmId = process.argv[2]; // Movie ID passed as a command line argument
-const apiUrl = {
-  url: 'https://swapi-api.alx-tools.com/api/films/' + filmId,
-  method: 'GET'
-};
 
-// Make a GET request to retrieve the movie data
-request(apiUrl, function (error, response, body) {
-  if (!error) {
-    // Extract the characters from the movie data
-    const characters = JSON.parse(body).characters;
-    printXterNames(characters, 0); // Start printing the character names
-  }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
-
-// Recursive function to print the characters' names
-function printXterNames (characters, index) {
-  // Make a GET request for each character URL
-  const characterUrl = characters[index];
-  request(characterUrl, function (error, response, body) {
-    if (!error) {
-      // Extract the character's name from the response data
-      const characterData = JSON.parse(body);
-      console.log(characterData.name);
-      // Check if there are more characters to print
-      if (index + 1 < characters.length) {
-        // Start printing recursively with the next character index
-        printXterNames(characters, index + 1);
-      }
-    }
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
   });
-}
+};
